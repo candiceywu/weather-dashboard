2a63b4a2589c00822ca2709df259c5ae30469f14
candiceywu <candice.ying.wu@gmail.com>
2021-08-24 09:55:38 -0700

    local storage saved

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,38 +2,181 @@ var searchTextEl = document.querySelector('#result-text');
 var resultContentEl = document.querySelector('#result-content');
 var searchFormEl = $('#search-form');
 var searchListEl = $('#search-list'); // for appending cities
-var searchBtn = $('.btn btn-info');
-var citySearch = $("#search-input");
+var searchBtn = $('#search-button');
+
 
 
 // logs entry into area under search button
 function handleFormSubmit(event) {
-  event.preventDefault();
-  var searchItem = $('input[name="search-input"]').val();
-  if (!searchItem) {
-    console.log('Please enter a city.');
-    return;
-  }
+    event.preventDefault();
+    var searchItem = $('input[name="search-input"]').val();
+    if (!searchItem) {
+        console.log('Please enter a city.');
+        return;
+    }
 
-  var searchListItemEl = $(
-    '<li class="flex-row justify-space-between align-center p-2 bg-white text-dark">'
-  );
-  searchListItemEl.text(searchItem);
+    var searchListItemEl = $(
+        '<li class="flex-row justify-space-between align-center p-2 bg-white text-dark">'
+    );
+    searchListItemEl.text(searchItem);
 
-  // print to the page
-  searchListEl.append(searchListItemEl);
+    // print to the page
+    searchListEl.append(searchListItemEl);
 
-//   // clear the form input element
-//   $('input[name="search-input"]').val('');
+    // clear the form input element after entry
+    $('input[name="search-input"]').val('');
+
 }
 
 // event delegation
 searchFormEl.on('submit', handleFormSubmit);
 
-
-//button clicks, local storage
-searchBtn.on("click", function(){
-  localStorage.setItem("city", (citySearch.val()));
+//button clicks, local storage DOES NOT WORK
+searchBtn.on("click", function (event) {
+    event.preventDefault();
+    var citySearch = $("#search-input").val();
+    localStorage.setItem("city", JSON.stringify(citySearch));
 });
 
 
+
+
+
+
+
+
+// //from mini-project
+
+// //script js
+// function handleSearchFormSubmit(event) {
+//   event.preventDefault();
+
+//   var searchInputVal = document.querySelector('#search-input').value;
+//   var formatInputVal = document.querySelector('#format-input').value;
+
+//   if (!searchInputVal) {
+//     console.error('You need a search input value!');
+//     return;
+//   }
+
+//   var queryString = './search-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
+
+//   location.assign(queryString);
+// }
+
+// searchFormEl.addEventListener('submit', handleSearchFormSubmit);
+
+
+
+
+// //display-search js
+// function getParams() {
+//   // Get the search params out of the URL (i.e. `?q=london&format=photo`) and convert it to an array (i.e. ['?q=london', 'format=photo'])
+//   var searchParamsArr = document.location.search.split('&');
+
+//   // Get the query and format values
+//   var query = searchParamsArr[0].split('=').pop();
+//   var format = searchParamsArr[1].split('=').pop();
+
+//   searchApi(query, format);
+// }
+
+// function printResults(resultObj) {
+//   console.log(resultObj);
+
+//   // set up `<div>` to hold result content
+//   var resultCard = document.createElement('div');
+//   resultCard.classList.add('card', 'bg-light', 'text-dark', 'mb-3', 'p-3');
+
+//   var resultBody = document.createElement('div');
+//   resultBody.classList.add('card-body');
+//   resultCard.append(resultBody);
+
+//   var titleEl = document.createElement('h3');
+//   titleEl.textContent = resultObj.title;
+
+//   var bodyContentEl = document.createElement('p');
+//   bodyContentEl.innerHTML =
+//     '<strong>Date:</strong> ' + resultObj.date + '<br/>';
+
+//   if (resultObj.subject) {
+//     bodyContentEl.innerHTML +=
+//       '<strong>Subjects:</strong> ' + resultObj.subject.join(', ') + '<br/>';
+//   } else {
+//     bodyContentEl.innerHTML +=
+//       '<strong>Subjects:</strong> No subject for this entry.';
+//   }
+
+//   if (resultObj.description) {
+//     bodyContentEl.innerHTML +=
+//       '<strong>Description:</strong> ' + resultObj.description[0];
+//   } else {
+//     bodyContentEl.innerHTML +=
+//       '<strong>Description:</strong>  No description for this entry.';
+//   }
+
+//   var linkButtonEl = document.createElement('a');
+//   linkButtonEl.textContent = 'Read More';
+//   linkButtonEl.setAttribute('href', resultObj.url);
+//   linkButtonEl.classList.add('btn', 'btn-dark');
+
+//   resultBody.append(titleEl, bodyContentEl, linkButtonEl);
+
+//   resultContentEl.append(resultCard);
+// }
+
+// function searchApi(query, format) {
+//   var locQueryUrl = 'https://www.loc.gov/search/?fo=json';
+
+//   if (format) {
+//     locQueryUrl = 'https://www.loc.gov/' + format + '/?fo=json';
+//   }
+
+//   locQueryUrl = locQueryUrl + '&q=' + query;
+
+//   fetch(locQueryUrl)
+//     .then(function (response) {
+//       if (!response.ok) {
+//         throw response.json();
+//       }
+
+//       return response.json();
+//     })
+//     .then(function (locRes) {
+//       // write query to page so user knows what they are viewing
+//       resultTextEl.textContent = locRes.search.query;
+
+//       console.log(locRes);
+
+//       if (!locRes.results.length) {
+//         console.log('No results found!');
+//         resultContentEl.innerHTML = '<h3>No results found, search again!</h3>';
+//       } else {
+//         resultContentEl.textContent = '';
+//         for (var i = 0; i < locRes.results.length; i++) {
+//           printResults(locRes.results[i]);
+//         }
+//       }
+//     })
+//     .catch(function (error) {
+//       console.error(error);
+//     });
+// }
+
+// function handleSearchFormSubmit(event) {
+//   event.preventDefault();
+
+//   var searchInputVal = document.querySelector('#search-input').value;
+//   var formatInputVal = document.querySelector('#format-input').value;
+
+//   if (!searchInputVal) {
+//     console.error('You need a search input value!');
+//     return;
+//   }
+
+//   searchApi(searchInputVal, formatInputVal);
+// }
+
+// searchFormEl.addEventListener('submit', handleSearchFormSubmit);
+
+// getParams();
