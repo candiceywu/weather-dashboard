fcd30d3059ba0f8f4289c36a3ab4c65fc433c46a
candiceywu <candice.ying.wu@gmail.com>
2021-08-25 10:18:06 -0700

    initial url

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,29 +7,30 @@ var citiesArray = [];
 
 
 
-//moment.js
+//moment.js for top of page
 var today = moment();
 $("#current-city-date").text(today.format("MMMM Do, YYYY"));
 
-//widget to show next 5 days
-var day1 = moment().add(1,'days');
-$("#day-1").text(day1.format("MMMM Do"));
+// //moment.js to show next 5 days
+// var day1 = moment().add(1, 'days');
+// $("#day-1").text(day1.format("MMMM Do"));
 
-var day2 = moment().add(2,'days');
-$("#day-2").text(day2.format("MMMM Do"));
+// var day2 = moment().add(2, 'days');
+// $("#day-2").text(day2.format("MMMM Do"));
 
-var day3 = moment().add(3,'days');
-$("#day-3").text(day3.format("MMMM Do"));
+// var day3 = moment().add(3, 'days');
+// $("#day-3").text(day3.format("MMMM Do"));
 
-var day4 = moment().add(4,'days');
-$("#day-4").text(day4.format("MMMM Do"));
+// var day4 = moment().add(4, 'days');
+// $("#day-4").text(day4.format("MMMM Do"));
 
-var day5 = moment().add(5,'days');
-$("#day-5").text(day5.format("MMMM Do"));
+// var day5 = moment().add(5, 'days');
+// $("#day-5").text(day5.format("MMMM Do"));
+
 
 
 //displays what's in local storage back on page
-function init () {
+function init() {
     var savedCities = JSON.parse(localStorage.getItem("city"));
     if (!savedCities) {
         return;
@@ -40,7 +41,7 @@ function init () {
             '<li class="flex-row justify-space-between align-center p-2 bg-white text-dark">'
         );
         searchListItemEl.text(citiesArray[i]);
-    
+
         // print to the page
         searchListEl.append(searchListItemEl);
     }
@@ -74,98 +75,39 @@ function handleFormSubmit(event) {
 // event delegation
 searchFormEl.on('submit', handleFormSubmit);
 
-init ();
+init();
 
 
 
 // fetch // must log in to create ID key (API key is your login a3837629950be0192cb4fdcba56908b4
 // )
-// api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
 
 
+//call current day: 
 
+var url = "api.openweathermap.org/data/2.5/weather?q=" + citSearch + "&appid=a3837629950be0192cb4fdcba56908b4"
 
+fetch(url)
+.then(function(response){
+    return response.json()
+})
+.then(function(data){
+    console.log(data)
+    var lon = data.coord.lon;
+    var lat = data.coord.lat;
+})
 
+var url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,minutely&appid=a3837629950be0192cb4fdcba56908b4"
+
+fetch(url)
+.then(function(response){
+    return response.json()
+})
+.then(function(data){
+    console.log(data)
+})
 
 // //from mini-project
-
-// //script js
-// function handleSearchFormSubmit(event) {
-//   event.preventDefault();
-
-//   var searchInputVal = document.querySelector('#search-input').value;
-//   var formatInputVal = document.querySelector('#format-input').value;
-
-//   if (!searchInputVal) {
-//     console.error('You need a search input value!');
-//     return;
-//   }
-
-//   var queryString = './search-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
-
-//   location.assign(queryString);
-// }
-
-// searchFormEl.addEventListener('submit', handleSearchFormSubmit);
-
-
-
-
-// //display-search js
-// function getParams() {
-//   // Get the search params out of the URL (i.e. `?q=london&format=photo`) and convert it to an array (i.e. ['?q=london', 'format=photo'])
-//   var searchParamsArr = document.location.search.split('&');
-
-//   // Get the query and format values
-//   var query = searchParamsArr[0].split('=').pop();
-//   var format = searchParamsArr[1].split('=').pop();
-
-//   searchApi(query, format);
-// }
-
-// function printResults(resultObj) {
-//   console.log(resultObj);
-
-//   // set up `<div>` to hold result content
-//   var resultCard = document.createElement('div');
-//   resultCard.classList.add('card', 'bg-light', 'text-dark', 'mb-3', 'p-3');
-
-//   var resultBody = document.createElement('div');
-//   resultBody.classList.add('card-body');
-//   resultCard.append(resultBody);
-
-//   var titleEl = document.createElement('h3');
-//   titleEl.textContent = resultObj.title;
-
-//   var bodyContentEl = document.createElement('p');
-//   bodyContentEl.innerHTML =
-//     '<strong>Date:</strong> ' + resultObj.date + '<br/>';
-
-//   if (resultObj.subject) {
-//     bodyContentEl.innerHTML +=
-//       '<strong>Subjects:</strong> ' + resultObj.subject.join(', ') + '<br/>';
-//   } else {
-//     bodyContentEl.innerHTML +=
-//       '<strong>Subjects:</strong> No subject for this entry.';
-//   }
-
-//   if (resultObj.description) {
-//     bodyContentEl.innerHTML +=
-//       '<strong>Description:</strong> ' + resultObj.description[0];
-//   } else {
-//     bodyContentEl.innerHTML +=
-//       '<strong>Description:</strong>  No description for this entry.';
-//   }
-
-//   var linkButtonEl = document.createElement('a');
-//   linkButtonEl.textContent = 'Read More';
-//   linkButtonEl.setAttribute('href', resultObj.url);
-//   linkButtonEl.classList.add('btn', 'btn-dark');
-
-//   resultBody.append(titleEl, bodyContentEl, linkButtonEl);
-
-//   resultContentEl.append(resultCard);
-// }
 
 // function searchApi(query, format) {
 //   var locQueryUrl = 'https://www.loc.gov/search/?fo=json';
