f184140bfd0e241a9ab3a53a4c2b47ce87282478
candiceywu <candice.ying.wu@gmail.com>
2021-08-29 22:33:24 -0700

    uploading readme

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # weather-dashboard
+
+
+## Description
+This application, Weather Dashboard, was created with the purpose of allowing the user to search for a city in the United States and see its current weather (including temperature, humidity, wind speed, and UV index) as well as the weather for the next five days. Once the user has searched for a city, it is saved in local storage as well as below the search bar so the user knows what cities they've searched for. The user can also click on a previously-searched city to quickly load its current and five-day forecast again. 
+
+## Issues
+This project was difficult for me because I could not figure out why my five-day forecast data would not replace one another with a new city search, and instead would add. I tried using .replaceWith and .text, but I think I needed to add in code to remove a child (but could not do this in time). Additionally, the buttons for each saved city below the search bar would display its corresponding weather information only after the user refreshed the page for the first time; it would not activate upon initial launch. Again, I couldn't figure out how to fix this in time.
+
+## Installation
+For installation and deployment, please [click here](https://candiceywu.github.io/weather-dashboard/). The application should launch but unfortunately as noted above, the previously-searched cities will not load its corresponding weather data unless the user refreshes the page once upon deployment (and after a city search).
+
+## Usage
+The user can easily search for a city to find out the current weather and five-day forecast for that city. The application also saves previous cities that the user searched for, so they can click on the saved city without re-entering text in the search bar to pull up weather information. The following image shows the web application's appearance:
+
+![Candice Wu's Weather Dashboard application includes a search bar on the left with weather information on the right (current and five-day forecast). The cities searched will automatically be saved/displayed on the page so the user can click on the city to reload weather information. The cities will display again if the page is closed/refreshed and opened again.](assets/images/screenshot.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,34 +2,25 @@ var searchFormEl = $('#search-form');
 var searchListEl = $('#search-list'); // for appending cities
 var searchBtn = $('#search-button');
 var citiesArray = [];
-
-
-
 //moment.js for top of page
 var today = moment();
 $("#current-day").text(today.format("MMMM Do, YYYY"));
-
 // //moment.js to show next 5 days
 var day1 = moment().add(1, 'days');
 $("#1").text(day1.format("MMMM Do"));
-
 var day2 = moment().add(2, 'days');
 $("#2").text(day2.format("MMMM Do"));
-
 var day3 = moment().add(3, 'days');
 $("#3").text(day3.format("MMMM Do"));
-
 var day4 = moment().add(4, 'days');
 $("#4").text(day4.format("MMMM Do"));
-
 var day5 = moment().add(5, 'days');
 $("#5").text(day5.format("MMMM Do"));
-
-
 
 //displays what's in local storage back on page
 function init() {
     var savedCities = JSON.parse(localStorage.getItem("city"));
+
     console.log(savedCities);
     if (!savedCities) {
         return;
@@ -41,32 +32,13 @@ function init() {
         );
         searchListItemEl.text(citiesArray[i]);
 
-        // //delete button to remove city
-        // searchListItemEl.append(
-        //     '<button class="btn btn-danger btn-small delete-item-btn">X</button>'
-        // );
-
         // print to the page
         searchListEl.append(searchListItemEl);
 
     }
 }
 
-
-// function handleRemoveItem(event) {
-//     // convert button we pressed (`event.target`) to a jQuery DOM object
-//     var btnClicked = $(event.target);
-
-//     // get the parent `<li>` element from the button we pressed and remove it
-//     btnClicked.parent('li').remove();
-// }
-
-// event delegation
-searchFormEl.on('submit', handleFormSubmit);
-// searchListEl.on('click', '.delete-item-btn', handleRemoveItem);
-
 init();
-
 
 // logs entry into area under search button
 function handleFormSubmit(event) {
@@ -82,11 +54,6 @@ function handleFormSubmit(event) {
     );
     searchListItemEl.text(searchItem);
 
-    // // add delete button to remove list item
-    // searchListItemEl.append(
-    //     '<button class="btn btn-danger btn-small delete-item-btn">X</button>'
-    // );
-
     // print to the page
     searchListEl.append(searchListItemEl);
     var citySearch = $("#search-input").val();
@@ -101,20 +68,27 @@ function handleFormSubmit(event) {
     generateWeatherHistory(searchItem);
 }
 
+// event delegation
+searchFormEl.on('submit', handleFormSubmit);
+
+
 
 //clicking city button will pull up previous weather value 
 var btnClassClick = function (e) {
     console.log(e.currentTarget.textContent);
     generateWeatherHistory(e.currentTarget.textContent);
 }
+
 $('.btn-block').on('click', btnClassClick);
 
 
 
 //fetching API data
 function generateWeatherHistory(searchItem) {
+
     //current day: 
     var url = "https://api.openweathermap.org/data/2.5/weather?q=" + searchItem + "&appid=a3837629950be0192cb4fdcba56908b4";
+
     fetch(url)
         .then(function (response) {
             return response.json()
@@ -126,7 +100,6 @@ function generateWeatherHistory(searchItem) {
 
             //use api with lon/lat and uv-index info
             var url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&exclude=hourly,minutely&appid=a3837629950be0192cb4fdcba56908b4";
-
             fetch(url)
                 .then(function (response) {
                     return response.json()
@@ -143,32 +116,36 @@ function generateWeatherHistory(searchItem) {
                     $("#wind-speed").text("Wind Speed: " + data.current.wind_speed + " mph");
                     $("#uv-index").text("UV-Index: " + data.current.uvi + " of 10");
                     generateFiveDayForecast(data.daily);
+
                     console.log(data.daily);
                 })
         })
-}
 
 
-
-//5-day forecast 
-function generateFiveDayForecast(data) {
-    console.log("data-daily", data);
-    var fiveDay = $("#five-day-forecast");
-    console.log(fiveDay);
-
-
-    for (let i = 1; i < 6; i++) {
-        var iconLink = $("<img>");
-        iconLink.attr("src", `https://openweathermap.org/img/w/${data[i].weather[0].icon}.png`);
-        iconLink.text("icon");
-        $(`#${i}`).append(iconLink);
-        $(`#${i}`).append("Temperature: " + `<div>${data[i].temp.day + "°F"}</div>`);
-        $(`#${i}`).append("Humidity: " + `<div>${data[i].humidity + "%"}</div>`);
-        $(`#${i}`).append("Wind Speed: " + `<div>${data[i].wind_speed + " mph"}</div>`);
-        $(`#${i}`).append("UV Index: " + `<div>${data[i].uvi + " of 10"}</div>`);
-
-        //find a way to remove children .children
+    //5-day forecast 
+    function generateFiveDayForecast(data) {
+        console.log("data-daily", data);
+        var fiveDay = $("#five-day-forecast");
+        console.log(fiveDay);
 
 
+        for (let i = 1; i < 6; i++) {
+            var iconLink = $("<img>");
+            iconLink.attr("src", `https://openweathermap.org/img/w/${data[i].weather[0].icon}.png`);
+            iconLink.text("icon");
+            $(`#${i}`).append(iconLink);
+            $(`#${i}`).append("Temperature: " + `<div>${data[i].temp.day + "°F"}</div>`);
+            $(`#${i}`).append("Humidity: " + `<div>${data[i].humidity + "%"}</div>`);
+            $(`#${i}`).append("Wind Speed: " + `<div>${data[i].wind_speed + " mph"}</div>`);
+            $(`#${i}`).append("UV Index: " + `<div>${data[i].uvi + " of 10"}</div>`);
+
+            //find a way to remove children .children
+
+
+        }
     }
 }
+
+
+
+
